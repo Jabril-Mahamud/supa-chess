f29db52f94321849d56a5cb67e67ec9b9fe12f96
Jabril Mahamud (personal account) <jabrilmahamud8@gmail.com>
2025-04-22 16:40:32 +0100

    fixed auth pages layout

--- app/(auth-pages)/layout.tsx
+++ app/(auth-pages)/layout.tsx
@@ -1,10 +1,9 @@
-// app/(auth-pages)/layout.tsx
 export default async function Layout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="max-w-7xl w-full flex flex-col gap-12 items-center justify-center min-h-[70vh]">{children}</div>
+    <div className="max-w-7xl flex flex-col gap-12 items-start">{children}</div>
   );
 }
